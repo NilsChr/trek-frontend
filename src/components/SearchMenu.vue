f173669b33a37111f64b0aa744bd8af44fbbbcac
Nils Christian Bogen <nils.chr.bogen@gmail.com>
2022-01-11 14:59:57 +0100

    testing github actions with retry

--- src/components/SearchMenu.vue
+++ src/components/SearchMenu.vue
@@ -3,7 +3,7 @@
     <error-500 v-if="error" />
     <v-layout column justify-space-between fill-height v-else>
       <v-layout column>
-        Test Docker - live with github actions
+        Test Docker - live with github actions2
         <v-flex xs2>
           <v-col cols="12">
             <v-img
